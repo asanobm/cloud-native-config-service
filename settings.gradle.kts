e54dac0236cbed0a08e0696651925154d793b156
asanobm <asanobm@outlook.com>
2024-08-21 06:55:32 +0000

    create config service

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "cloud-native-config-service"
---
+++ src/test/kotlin/io/hannahsoft/cloudnativeconfigservice/CloudNativeConfigServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.hannahsoft.cloudnativeconfigservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudNativeConfigServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
